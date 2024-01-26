8d84d2145247d0ef707a84ef59b934e4281176e2
juanjo <juanjopalma03@gmail.com>
2024-01-26 09:29:05 +0100

    structure with routes, navbar and footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import { AppRoutes } from './Routes/AppRoutes';
+
+function App() {
+
+
+  return (
+    <>
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
---
+++ src/Routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Row } from 'react-bootstrap';
+import { NavbarApp } from '../components/NavbarApp/NavbarApp';
+import { Footer } from '../components/FooterApp/Footer';
+import { Home } from '../pages/dashboard/Home/Home';
+
+export const AppRoutes = () => {
+
+  return (
+    <BrowserRouter>
+        <NavbarApp />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+  )
+}
